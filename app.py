6bb4b09f39e3e32bf51ed8c71fa87d1e96b9b5bd
Paul Rosa <paulirosa20@gmail.com>
2024-02-24 20:48:10 -0500

    renamed docs from templates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import openai
 
-app = Flask(__name__, template_folder = 'templates')
+app = Flask(__name__, template_folder = 'docs')
 
 
 openai.api_key = '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'
